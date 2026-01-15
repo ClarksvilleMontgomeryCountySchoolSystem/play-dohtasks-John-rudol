bac8b38899cb761654f18edb05550819b7ae8557
John-rudol <johnathan.rudolph097@students.cmcss.net>
2026-01-15 14:13:08 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -14,10 +14,10 @@ def main():
     if choice2 == "long tail":
      print(f"5) Roll a thin rope using {color2} and attach to the back.\n")
     else:
-    print(f"5) Add a small bump using {color2} to the back.\n")
-    print(f"6) Add four small legs to the bottom using {color1}.\n ") 
+     print(f"5) Add a small bump using {color2} to the back.\n")
+     print(f"6) Add four small legs to the bottom using {color1}.\n")
     print("7) Add two dots for eyes and a tiny nose.\n")
-    print(') Name this creation: "Mouse"')
+    print( '8) Name this creation: "Mouse"')
 
 
 if __name__ == "__main__":
