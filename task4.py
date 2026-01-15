30352174c6cac462961453a2a1d04306c845d6a5
John-rudol <johnathan.rudolph097@students.cmcss.net>
2026-01-15 08:02:45 -0600

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,8 +1,25 @@
+
+"""
+ANSWER BOX - Copy the strings you need:
+
+"standing up or flopped down?"
+") Keep it round.\n"
+") Attach two pieces using {color2} hanging downward.\n"
+") Name this creation: "Dog""
+") Roll a smaller ball using {color1} for the head.\n"
+"hot dog or round like a ball?"
+") Roll a ball using {color1} for the body.\n"
+") Attach two pointed pieces using {color2} upright.\n"
+") Attach the head to the body.\n"
+") Add four legs using {color1}, a small tail using {color2}, two eyes, and a nose.\n"
+") Stretch it out.\n"
+"""
+
 def main():
     color1 = "brown"
     color2 = "tan"
     print(f"1) Roll a ball using {color1} for the body.\n")
-    choice1 = input("long body or round body? ")
+    choice1 = input("hot dog or round like a ball? ")
     if choice1 == "hot dog":
         print("2) Stretch it out.\n")
     else:
@@ -19,4 +36,3 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
